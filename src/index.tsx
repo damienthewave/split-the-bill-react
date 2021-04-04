9dad0c6056e6e145b83cfb43ee3f09d97f4814ed
Kacper Wojcikowski <wojcikowskikacper5@gmail.com>
2021-04-04 12:51:30 +0200

    Added react bootstrap dependency

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./components/App";
 import { configureStore } from "./redux/configureStore";
 import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const store = configureStore();
 
